62a09b10eb9761feaa764f6ccc305616c903d1dd
DreamlandSR <ryanidb35@gmail.com>
2026-03-19 13:25:38 +0700

    fix: paksa session dan cache driver ke file

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,11 @@ fi
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
+# Paksa session & cache pakai file
+sed -i 's/SESSION_DRIVER=database/SESSION_DRIVER=file/' /var/www/.env
+sed -i 's/CACHE_STORE=database/CACHE_STORE=file/' /var/www/.env
+
+php artisan migrate --force
+php artisan config:clear
+
 exec "$@"
