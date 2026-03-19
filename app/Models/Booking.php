8ed19e9f2d590b42d38769fe156786642d6471c9
DreamlandSR <ryanidb35@gmail.com>
2026-03-19 13:12:19 +0700

    feat: tambah model Kamar, GaleriKamar, Booking, Tagihan

---
+++ app/Models/Booking.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    protected $table = 'booking';
+    protected $primaryKey = 'id_booking';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_user', 'id_kamar', 'tgl_booking',
+        'durasi_sewa_bulan', 'tgl_mulai_sewa',
+        'tgl_akhir_sewa', 'total_biaya_bulanan', 'status_booking'
+    ];
+
+    public function kamar()
+    {
+        return $this->belongsTo(Kamar::class, 'id_kamar', 'id_kamar');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user', 'id');
+    }
+
+    public function tagihan()
+    {
+        return $this->hasMany(Tagihan::class, 'id_booking', 'id_booking');
+    }
+}
