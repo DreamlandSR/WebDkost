8df4bfcf20b492b815c981bcd2c8cddf7b4272e6
DreamlandSR <ryanidb35@gmail.com>
2026-03-24 22:50:34 +0700

    fix : UserFactory user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,12 +15,10 @@ class UserFactory extends Factory
         return [
             'nama' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
-            'no_hp' => $this->faker->phoneNumber,
-            'password' => bcrypt('password'), // bisa diganti pakai Hash::make jika pakai helper
-            'otp' => str_pad(strval(rand(0, 999999)), 6, '0', STR_PAD_LEFT),
-            'otp_expiry' => now()->addMinutes(10),
-            'role' => $this->faker->randomElement(['admin', 'user']),
-            'avatar' => $this->faker->imageUrl(300, 300, 'people', true, 'Avatar'),
+            'password' => bcrypt('password'),
+            'no_telepon' => $this->faker->phoneNumber,
+            'alamat' => $this->faker->address,
+            'role' => $this->faker->randomElement(['admin', 'penyewa']),
             'created_at' => now(),
         ];
     }
