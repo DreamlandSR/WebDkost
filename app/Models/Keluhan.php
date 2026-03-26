18dee7aae13c00c93ea3b9e2fe6cc763b8b19aa8
ninoandi <ninoandi2002@gmail.com>
2026-03-26 17:59:00 +0700

    p
    p

--- app/Models/Keluhan.php
+++ app/Models/Keluhan.php
@@ -1,12 +1,34 @@
 <?php
+
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Keluhan extends Model {
+class keluhan extends Model
+{
+    use HasFactory;
+
     protected $table = 'keluhan';
     protected $primaryKey = 'id_keluhan';
-    public $timestamps = false;
-    protected $fillable = ['id_user','id_kamar','deskripsi_masalah','foto_bukti','tgl_lapor','status_keluhan'];
+    public $timestamps = false; 
 
-    public function kamar() { return $this->belongsTo(Kamar::class, 'id_kamar', 'id_kamar'); }
+    protected $fillable = [
+        'id_user',
+        'id_kamar',
+        'deskripsi_masalah',
+        'foto_bukti',
+        'tgl_lapor',
+        'status_keluhan',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user', 'id_user');
+    }
+
+    public function kamar()
+    {
+        return $this->belongsTo(Kamar::class, 'id_kamar', 'id_kamar');
+    }
 }
