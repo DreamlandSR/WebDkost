e4bd3ffd0666eba0f8d597a7dab052a7d0cdcf84
Fputradf <148319781+Fputradf@users.noreply.github.com>
2026-03-30 18:59:08 +0700

    tes

--- app/Models/keluhan.php
+++ app/Models/keluhan.php
@@ -1,36 +1,12 @@
 <?php
-
 namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
-class Keluhan extends Model
-{
-    use HasFactory;
-
+class Keluhan extends Model {
     protected $table = 'keluhan';
     protected $primaryKey = 'id_keluhan';
-
     public $timestamps = false;
+    protected $fillable = ['id_user','id_kamar','deskripsi_masalah','foto_bukti','tgl_lapor','status_keluhan'];
 
-    protected $fillable = [
-        'id_user',
-        'id_kamar',
-        'deskripsi_masalah',
-        'foto_bukti',
-        'tgl_lapor',
-        'status_keluhan',
-    ];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class, 'id_user', 'id_user');
-    }
-
-    public function kamar()
-    {
-        return $this->belongsTo(Kamar::class, 'id_kamar', 'id_kamar');
-    }
+    public function kamar() { return $this->belongsTo(Kamar::class, 'id_kamar', 'id_kamar'); }
 }
